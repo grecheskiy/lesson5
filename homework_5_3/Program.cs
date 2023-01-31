f4899b6184207db13241f13fa10065c6af93192e
grecheskiy <grecheskiy@rambler.ru>
2023-01-31 23:29:03 +0300

    add homework_5_3

---
+++ homework_5_3/Program.cs
@@ -0,0 +1,14 @@
+﻿//Написать программу копирования массива
+Console.Write("Enter array size: ");
+int k = int.Parse(Console.ReadLine()?? "");
+int[] arr = new int[k];
+for (int i=0; i<arr.Length; i++)
+{int rnd = new Random().Next(-99,99);
+arr[i] = rnd;
+Console.Write($"{arr[i]} ");}
+Console.WriteLine(); 
+int c = arr.Length;
+int[] copy = new int[c];
+for (int j=0; j<c; j++) 
+{copy[j] = arr[j];}
+Console.WriteLine(String.Join(" ", copy));
